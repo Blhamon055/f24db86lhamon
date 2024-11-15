01afedba2ac9afcabe6827e6ddf91b539b4b92f1
Braeden Lhamon <S543387@nwmissouri.edu>
2024-11-14 18:58:25 -0600

    Assignment #11 Fixing step #3

--- routes/frappacino.js
+++ routes/frappacino.js
@@ -3,5 +3,5 @@ const frappacino_controller = require('../controllers/frappacino');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/frappacino/:id', frappacino_controller.frappacino_detail);
+router.get('/frappacino/:id', frappacino_controller.frappacino_update_put);
 module.exports = router;
