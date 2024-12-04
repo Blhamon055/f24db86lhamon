c1eccb0a6db3849b278e8e87724ff0f1c805880c
Braeden Lhamon <S543387@nwmissouri.edu>
2024-12-04 17:44:41 -0600

    Completed Assignment #13

--- models/frappacino.js
+++ models/frappacino.js
@@ -2,7 +2,11 @@ const mongoose = require("mongoose")
 const frappacinoSchema = mongoose.Schema({
 	size: String,
 	brand: String,
-	orderNum: Number
+	orderNum: {
+		type: Number,
+		min: 0o0,
+		max: 999,
+	}
 })
 
 module.exports = mongoose.model("Frappacino", frappacinoSchema)
